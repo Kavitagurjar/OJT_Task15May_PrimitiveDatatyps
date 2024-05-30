0879ef1df49935d23ea557c2e6a0551136606178
Kavita Gurjar <121787703+Kavitagurjar@users.noreply.github.com>
2024-05-30 16:07:52 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,41 @@
+public class Primitive {
+
+    public static void main(String[] args) {
+        System.out.println("Kavita Gurjar");
+
+        // PRIMITIVE DATATYPES 
+
+        // INT
+        int a = 23;
+        System.out.println(a + " is a integer.");
+
+        // FLOAT
+        float b = 8.5f;
+        System.out.println(b + " is a float.");
+
+        // BYTE 
+        byte c = -119;
+        System.out.println(c + " is a byte.");
+
+        // DOUBLE
+        double d = 99.5;
+        System.out.println(d + " is a double.");
+
+        // BOOLEAN
+        boolean e = true;
+        System.out.println(e   + " is a boolean.");
+
+        // CHAR
+        char f = 'k';
+        System.out.println(f + " is a char.");
+
+        // SHORT
+        short g = 56;
+        System.out.println(g + " is a short.");
+
+        // LONG
+        long h = 1234567890;
+        System.out.println(h + " is a long.");
+    }
+    
+}
